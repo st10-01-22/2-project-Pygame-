55649b1469811192d12171a05c5d76907fe6a582
Nowman <gebndlk@gmail.com>
2023-01-09 19:22:00 +0500

    Прототип

--- main.py
+++ main.py
@@ -56,6 +56,71 @@ def start_screen():
         clock.tick(FPS)
 
 
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    return new_player, x, y
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        if tile_type == 'wall':
+            super().__init__(tiles_group, all_sprites, barriers_group)
+            self.image = tile_images[tile_type]
+            self.rect = self.image.get_rect().move(
+                tile_width * pos_x, tile_height * pos_y)
+        else:
+            super().__init__(tiles_group, all_sprites)
+            self.image = tile_images[tile_type]
+            self.rect = self.image.get_rect().move(
+                tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+
+    def move(self, new_x, new_y):
+        self.rect.x = new_x
+        self.rect.y = new_y
+
+
+class Camera:
+    # зададим начальный сдвиг камеры
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    # сдвинуть объект obj на смещение камеры
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    # позиционировать камеру на объекте target
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)
+
+
 if __name__ == "__main__":
     size = width, height = 1500, 700
     screen = pygame.display.set_mode(size)
@@ -65,16 +130,38 @@ if __name__ == "__main__":
     tiles_group = pygame.sprite.Group()
     barriers_group = pygame.sprite.Group()
     player = None
+    dist = 100
     FPS = 50
+    tile_width = tile_height = 32
+    tile_images = {
+        'wall': load_image('trava.png'),
+        'empty': load_image('gorka_pr.png')
+    }
+    player_image = load_image('lich.png')
+    player, level_x, level_y = generate_level(load_level('map.txt'))
     pygame.init()
     running = True
+    camera = Camera()
     start_screen()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            key = pygame.key.get_pressed()
+            if key[pygame.K_DOWN]:
+                player.move(player.rect.x, player.rect.y + dist)
+            if key[pygame.K_UP]:
+                player.move(player.rect.x, player.rect.y - dist)
+            if key[pygame.K_RIGHT]:
+                player.move(player.rect.x + dist, player.rect.y)
+            if key[pygame.K_LEFT]:
+                player.move(player.rect.x - dist, player.rect.y)
+        screen.fill(pygame.Color("black"))
         all_sprites.draw(screen)
         tiles_group.draw(screen)
+        camera.update(player)
+        for sprite in all_sprites:
+            camera.apply(sprite)
         player_group.draw(screen)
         all_sprites.update()
         pygame.display.flip()
